acbe7dedd15b7c9bc4a3205b6cab867350171a3a
Manohar0369 <124118539+Manohar0369@users.noreply.github.com>
2024-01-03 02:43:59 +0000

    modified word count

--- Day-02/examples/01-string-len.py
+++ Day-02/examples/01-string-len.py
@@ -1,3 +1,6 @@
 text = "Python is awesome"
 length = len(text)
 print("Length of the string:", length)
+words = text.split() # use when you want to print the number of words in the sentense 
+num_words = len(words)
+print("words in the text:", num_words)
